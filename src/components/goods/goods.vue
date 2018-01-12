bf02c8a60781e7aa7a6bf872eb4852fd91505fd9
Administrator <1163181724@qq.com>
2018-01-12 11:02:16 +0800

    top部分

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
     <div class="goods">
         <div class="menu-wrapper" ref="menuwrapper">
           <ul>
-            <li v-for="item in goods" class="menu-item ">
+            <li v-for="(item,index) in goods" class="menu-item " :class="{'current':currentIndex===index}">
               <span class="text border-1px">
                 <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
               </span>
@@ -47,7 +47,8 @@
       data(){
         return{
           goods:[],
-          listHeight:[]
+          listHeight:[],
+          scrollY:0
         }
       },
       props:{
@@ -61,18 +62,35 @@
           this.goods=res.data.goods;
           this.$nextTick(()=>{
             this._initScroll();
-
-
+            this._calculateHeight();
           });
         })
+      },
+      computed:{
+        currentIndex(){
+          for(let i=0;i<this.listHeight.length;i++){
+            let height1=this.listHeight[i];
+            let height2=this.listHeight[i+1];
+            if(!height2 || (this.scrollY >= height1 && this.scrollY <height2)){
+
+              return i;
+
+            }
+          }
+
+          return 0;
+        }
       },
       methods:{
         _initScroll(){
           this.menuScroll=new BScroll(this.$refs.menuwrapper,{click:true});
-          this.foodsCroll=new BScroll(this.$refs.foodwrapper,{click:true});
+          this.foodScroll=new BScroll(this.$refs.foodwrapper,{probeType:3});//probeType监听实时滚动的位置
+          this.foodScroll.on('scroll',(pos)=>{
+            this.scrollY=Math.abs(Math.round(pos.y));
+          })
         },
         _calculateHeight(){
-          let foodList=this.refs.foodwrapper.getElementsByClassName('food-list-hook');
+          let foodList=this.$refs.foodwrapper.getElementsByClassName('food-list-hook');
           let height=0;
           this.listHeight.push(height);
           for(let i=0;i<foodList.length;i++){
@@ -107,6 +125,17 @@
         width: 56px;
         line-height: 14px;
         padding: 0 12px;
+        &.current{
+          position: relative;
+          z-index: 10;
+          margin-top: -1px;
+          background: #fff;
+          font-weight: 700;
+          .text{
+            .border-none();
+          }
+        }
+
         .icon{
           display: inline-block;
           vertical-align: top;
