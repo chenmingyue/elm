b8074dcac23bfcec9ce9277c5195a37634124eb9
Administrator <1163181724@qq.com>
2018-02-08 17:16:07 +0800

    douban

--- src/components/seller/seller.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>seller</div>
-</template>
-
-<script type="text/ecmascript-6">
-    export default {}
-</script>
-
-<style  scoped lang="less"></style>
--- src/mock.js
+++
@@ -1,8 +0,0 @@
-//引入mockjs
-const Mock = require('mockjs')
-//使用mockjs模拟数据
-Mock.mock('./api/data.json', (req, res) => {
-  return {
-    data: res
-  }
-})
